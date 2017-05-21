8ffa4f12e84ab006be96a24affc78c992b8c36ad
Jan Rychlik <jan.rychlik@improvisio.cz>
2017-05-21 19:46:11 +0200

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,18 +14,34 @@ app.use(express.static(publicPath));
 io.on('connection', function(socket) {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app.',
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        created_at: new Date().getTime()
+    });
+
     socket.on('disconnect', function() {
         console.log('client has disconnected');
     });
 
-    socket.emit('newMessage', {
-        from: 'mike',
-        text: 'yolo',
-        created_at: new Date()
-    });
-
     socket.on('createMessage', function(message) {
         console.log('createMessage', message);
+
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     created_at: new Date().getTime()
+        // });
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            created_at: new Date().getTime()
+        });
 
     });
 });
